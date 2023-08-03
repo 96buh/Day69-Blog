e03346b07c065788bc4d1ee74cad52ee4223d515
Homer <j29823678@gmail.com>
2023-08-04 01:03:23 +0800

    some small change

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ import os
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+# "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -22,8 +23,9 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://blogpost_jjzr_user:xiYfcepuRLYDEYhUb9oFaE4nDCXVYFMj@dpg-cj5s4mpitvpc738amifg-a.singapore-postgres.render.com/blogpost_jjzr"
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+# "postgresql://blogpost_jjzr_user:xiYfcepuRLYDEYhUb9oFaE4nDCXVYFMj@dpg-cj5s4mpitvpc738amifg-a.singapore-postgres.render.com/blogpost_jjzr"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///posts.db"
 db = SQLAlchemy()
 db.init_app(app)
 
@@ -45,7 +47,7 @@ class BlogPost(db.Model):
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    img_url = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String, nullable=False)
     #
     author_id = mapped_column(ForeignKey("user.id"))
     author = relationship("User", back_populates="posts")
